81d4ce1f2a3ee3ea0c889ae1c9257e1423973f71
Asi <asierrosa@gmail.com>
2020-11-05 13:27:10 +0100

    algunos cambios

--- src/main/java/ehu/isad/controller/ui/HasieraKud.java
+++ src/main/java/ehu/isad/controller/ui/HasieraKud.java
@@ -1,13 +1,20 @@
 package ehu.isad.controller.ui;
 
 import ehu.isad.EurovisionEIB;
+import ehu.isad.utils.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import ehu.isad.utils.Utils;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
 
 public class HasieraKud implements Initializable {
 
@@ -21,12 +28,27 @@ public class HasieraKud implements Initializable {
     private Button btn_bozkatu;
 
     @FXML
+    private ImageView imgIrudia;
+
+    @FXML
     void klikEgin(ActionEvent event) {
 
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        imgIrudia.setImage(setIcon("/resources/hasiera.png"));
+    }
 
+    public ImageView setIcon(String path) {
+
+        InputStream is=Utils.class
+                .getResourceAsStream(path);
+        ImageView iv = new ImageView(new Image(is));
+
+        iv.setFitWidth(100);
+        iv.setFitHeight(100);
+        Image image = SwingFXUtils.toFXImage(iv, null);
+        return image;
     }
 }
